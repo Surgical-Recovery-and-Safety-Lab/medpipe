8b9ead9abe7c573577c0cc2be46d12262d6c8ba8
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-10-31 15:56:17 +1300

    Updated test file to include tests for configuration reading

--- test/test_utils_io.py
+++ test/test_utils_io.py
@@ -6,6 +6,7 @@ test_utils.io.py
 Test functions for the utils.io module
 """
 import pathlib
+import tomllib
 
 import pytest
 
@@ -33,3 +34,30 @@ def test_load_data_from_csv_not_csv_file():
 def test_load_data_from_csv_file_not_found():
     with pytest.raises(FileNotFoundError):
         load_data_from_csv("not_a_file.csv")
+
+
+def test_read_toml_configuration_success():
+    data_path = str(CWD / "test/test_data/test_config.toml")
+    read_toml_configuration(data_path)
+
+
+def test_read_toml_configuration_not_str():
+    with pytest.raises(TypeError):
+        read_toml_configuration(12)
+
+
+def test_read_toml_configuration_not_toml_file():
+    with pytest.raises(ValueError):
+        data_path = str(CWD / "test/test_data/test_text.txt")
+        read_toml_configuration(data_path)
+
+
+def test_read_toml_configuration_incorrect_toml_file():
+    with pytest.raises(tomllib.TOMLDecodeError):
+        data_path = str(CWD / "test/test_data/not_a_good_toml.toml")
+        read_toml_configuration(data_path)
+
+
+def test_read_toml_configuration_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        read_toml_configuration("not_a_file.toml")
