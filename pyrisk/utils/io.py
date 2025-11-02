d285fb41b7f7901d1f57a3c464df8c5cddeab089
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-03 11:51:09 +1300

    Wrote a function to save data to csv file

--- pyrisk/utils/io.py
+++ pyrisk/utils/io.py
@@ -5,8 +5,9 @@ This module provides helper functions for reading from and writing to files,
 handling various common I/O tasks.
 
 Functions:
-- load_data_from_csv: Loads the data from a csv file.
-- read_toml_configuration: Parses the contents of a TOML file.
+- load_data_from_csv: Loads the data from a .csv file.
+- save_data_to_csv: Saves a DataFrame or Series to a .csv file.
+- read_toml_configuration: Parses the contents of a .TOML file.
 """
 
 import tomllib
@@ -23,7 +24,7 @@ def load_data_from_csv(data_path: str):
     Parameters
     __________
     data_path
-        Path to the csv file to load.
+        Path to the .csv file to load.
 
     Returns
     _______
@@ -47,6 +48,46 @@ def load_data_from_csv(data_path: str):
 
     data = pd.read_csv(data_path)
     return data
+
+
+def save_data_to_csv(df: pd.DataFrame, file_path: str) -> None:
+    """
+    Save data from a DataFrame to a .csv file.
+
+    Parameters:
+    ----------
+    df
+        DataFrame to save.
+    file_path
+        Path and file name to save to.
+
+    Returns:
+    -------
+    None
+        Nothing is returned.
+
+    Raises:
+    ------
+    TypeError
+        If file_path is not a str.
+    TypeError
+        If df is not a pd.DataFrame or pd.Series.
+    ValueError
+        If file_path extension is not .csv file.
+
+    """
+    try:
+        exceptions.path_checks(file_path, ".csv")
+    except (TypeError, ValueError):
+        raise
+    except FileNotFoundError:
+        # File will be created so skip FileNotFoundError
+        pass
+
+    if type(df) is not pd.DataFrame:
+        raise TypeError("df should be a pd.DataFrame")
+
+    df.to_csv(file_path)
 
 
 def read_toml_configuration(config_path: str) -> dict:
