6a057fc9c4c832bdc94f1507f9573184043d4d36
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-03 15:40:26 +1300

    Added test for path_checks function

--- test/test_utils_exceptions.py
+++ test/test_utils_exceptions.py
@@ -9,7 +9,7 @@ import pathlib
 
 import pytest
 
-from pyrisk.utils.exceptions import file_checks
+from pyrisk.utils.exceptions import file_checks, path_checks
 
 CWD = pathlib.Path.cwd()
 DATA_DIR = str(CWD / "test/test_data/")
@@ -24,8 +24,8 @@ DATA_DIR = str(CWD / "test/test_data/")
     ],
 )
 def test_file_checks_success(file_name, extension):
-    data_path = str(CWD / DATA_DIR / file_name)
-    file_checks(data_path, extension)
+    file_path = str(CWD / DATA_DIR / file_name)
+    file_checks(file_path, extension)
 
 
 def test_file_checks_not_str():
@@ -43,8 +43,8 @@ def test_file_checks_not_str():
 )
 def test_file_checks_not_extension_file(file_name, extension):
     with pytest.raises(ValueError):
-        data_path = str(CWD / DATA_DIR / file_name)
-        file_checks(data_path, extension)
+        file_path = str(CWD / DATA_DIR / file_name)
+        file_checks(file_path, extension)
 
 
 @pytest.mark.parametrize(
@@ -71,3 +71,32 @@ def test_file_checks_file_not_found(file_name, extension):
 def test_file_checks_not_a_file(extension):
     with pytest.raises(IsADirectoryError):
         file_checks(DATA_DIR, extension)
+
+
+def test_path_checks_success():
+    path_checks(DATA_DIR)
+
+
+def test_path_checks_not_str():
+    with pytest.raises(TypeError):
+        path_checks(12)
+
+
+def test_path_checks_dir_not_found():
+    with pytest.raises(FileNotFoundError):
+        data_path = str(CWD / "not_a_dir/")
+        path_checks(data_path)
+
+
+@pytest.mark.parametrize(
+    "file_name",
+    [
+        ("test_text.txt"),
+        ("test_data.csv"),
+        ("test_config.toml"),
+    ],
+)
+def test_path_checks_is_a_file(file_name):
+    with pytest.raises(NotADirectoryError):
+        data_path = str(CWD / DATA_DIR / file_name)
+        path_checks(data_path)
