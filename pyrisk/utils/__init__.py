da04003d6129fb50c06c7d509975cb9eb03c3751
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-03 15:41:22 +1300

    Updated __init__ docstring

--- pyrisk/utils/__init__.py
+++ pyrisk/utils/__init__.py
@@ -4,7 +4,9 @@ pyrisk.utils module
 submodules:
 - io: contains I/O functions.
 - exceptions: contains exceptions handling functions.
+- logger: contains logging functions.
 """
 
 import pyrisk.utils.exceptions
 import pyrisk.utils.io
+import pyrisk.utils.logger
