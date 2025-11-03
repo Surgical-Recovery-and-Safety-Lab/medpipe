fe6d85470d4f3cf263778a631e049bde441481c5
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-03 15:14:52 +1300

    Updated the exception functions
    Distinguished between file and path checks

--- pyrisk/utils/exceptions.py
+++ pyrisk/utils/exceptions.py
@@ -10,16 +10,57 @@ Functions:
 import pathlib
 
 
-def path_checks(path: str, extension: str) -> None:
+def file_checks(file: str, extension: str) -> None:
     """
-    Performs checks to ensure that a path and extension are correct.
+    Performs checks to ensure that a file and extension are correct.
+
+    Parameters
+    ----------
+    file
+        File to check.
+    extension
+        Extension of the file to check.
+
+    Returns
+    -------
+    None
+        Nothing is returned.
+
+    Raises
+    ------
+    TypeError
+        If file is not a str.
+    FileNotFoundError
+        If file does not exist.
+    IsADirectoryError
+        If file is a directory.
+    ValueError
+        If file extension is not .extension file.
+
+    """
+    if type(file) is not str:
+        raise TypeError(f"{file} should be a string")
+
+    path_object = pathlib.Path(file)  # Create a Path object
+
+    if not path_object.exists():
+        raise FileNotFoundError(f"{file} does not exist")
+
+    if not path_object.is_file():
+        raise IsADirectoryError(f"{file} should be a file")
+
+    if path_object.suffix != extension:
+        raise ValueError(f"{file} should be a {extension} file")
+
+
+def path_checks(path: str) -> None:
+    """
+    Performs checks to ensure that a path is correct.
 
     Parameters
     ----------
     path
-        Path to the file to check.
-    extension
-        Extension of the file to check.
+        Path to check.
 
     Returns
     -------
@@ -32,8 +73,8 @@ def path_checks(path: str, extension: str) -> None:
         If path is not a str.
     FileNotFoundError
         If path does not exist.
-    ValueError
-        If path extension is not .extension file.
+    NotADirectoryError
+        If path is not a directory.
 
     """
     if type(path) is not str:
@@ -44,5 +85,5 @@ def path_checks(path: str, extension: str) -> None:
     if not path_object.exists():
         raise FileNotFoundError(f"{path} does not exist")
 
-    if path_object.suffix != extension:
-        raise ValueError(f"{path} should be a {extension} file")
+    if not path_object.is_dir():
+        raise NotADirectoryError(f"{path} should be a directory")
