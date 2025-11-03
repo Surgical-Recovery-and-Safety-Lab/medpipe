1b02193746e2bca682dea154f5f1a2d713ddc2a0
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-03 15:15:14 +1300

    Updated functions to use file_checks

--- pyrisk/data/db.py
+++ pyrisk/data/db.py
@@ -14,16 +14,16 @@ import pyrisk.utils.exceptions
 
 
 def parquet_to_duckdb(
-    parquet_path: str, duckdb_path: str, table_name: str = "main"
+    parquet_file: str, duckdb_file: str, table_name: str = "main"
 ) -> None:
     """
     Converts a .parquet file to a .duckdb file.
 
     Parameters
     ----------
-    parquet_path
+    parquet_file
         File path to the .parquet file.
-    duckdb_path
+    duckdb_file
         File path to the .duckdb file.
     table_name, default: 'main'
         Name of the table to create in the duckdb database.
@@ -36,32 +36,34 @@ def parquet_to_duckdb(
     Raises
     ------
     TypeError
-        If parquet_path or duckdb_path are not str.
+        If parquet_file or duckdb_file are not str.
     FileNotFoundError
-        If parquet_path or duckdb_path do not exist.
+        If parquet_file does not exist.
+    IsADirectoryError
+        If parquet_file or duckdb_file are not a file.
     ValueError
-        If parquet_path extension is not a .parquet file.
+        If parquet_file extension is not a .parquet file.
     ValueError
-        If duckdb_path extension is not a .duckdb file.
+        If duckdb_file extension is not a .duckdb file.
 
     """
     try:
-        pyrisk.utils.exceptions.path_checks(parquet_path, ".parquet")
-    except (FileNotFoundError, TypeError, ValueError):
+        pyrisk.utils.exceptions.file_checks(parquet_file, ".parquet")
+    except (FileNotFoundError, IsADirectoryError, TypeError, ValueError):
         raise
 
     try:
-        pyrisk.utils.exceptions.path_checks(duckdb_path, ".duckdb")
-    except (TypeError, ValueError):
+        pyrisk.utils.exceptions.file_checks(duckdb_file, ".duckdb")
+    except (TypeError, ValueError, IsADirectoryError):
         raise
     except FileNotFoundError:
         # Duckdb will be created so pass this exception
         pass
 
-    conn = duckdb.connect(duckdb_path)
+    conn = duckdb.connect(duckdb_file)
 
     drop_table = f"DROP TABLE IF EXISTS {table_name}"
-    query = f"CREATE TABLE {table_name} AS SELECT * FROM '{parquet_path}'"
+    query = f"CREATE TABLE {table_name} AS SELECT * FROM '{parquet_file}'"
 
     # Execute the queries
     conn.execute(drop_table)
@@ -69,13 +71,13 @@ def parquet_to_duckdb(
     conn.close()
 
 
-def extract_data_from_duckdb(duckdb_path: str, query: str):
+def extract_data_from_duckdb(duckdb_file: str, query: str):
     """
     Extracts data from a duckdb and saves it to a .csv file.
 
     Parameters
     ----------
-    duckdb_path
+    duckdb_file
         Path to the duckdb file.
     query
         Query to send to the duckdb to extract data.
@@ -88,22 +90,24 @@ def extract_data_from_duckdb(duckdb_path: str, query: str):
     Raises
     ------
     TypeError
-        If duckdb_path or query is not a str.
+        If duckdb_file or query is not a str.
     FileNotFoundError
-        If duckdb_path does not exist.
+        If duckdb_file does not exist.
+    IsADirectoryError
+        If duckdb_file is not a file.
     ValueError
-        If duckdb_path extension is not a .duckdb file.
+        If duckdb_file extension is not a .duckdb file.
 
     """
     try:
-        pyrisk.utils.exceptions.path_checks(duckdb_path, ".duckdb")
-    except (FileNotFoundError, TypeError, ValueError):
+        pyrisk.utils.exceptions.file_checks(duckdb_file, ".duckdb")
+    except (FileNotFoundError, IsADirectoryError, TypeError, ValueError):
         raise
 
     if type(query) is not str:
         raise TypeError(f"{query} should be a string")
 
-    conn = duckdb.connect(duckdb_path)
+    conn = duckdb.connect(duckdb_file)
 
     df = conn.execute(query).df()  # Get the data
     conn.close()
