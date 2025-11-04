837f9b3dacb6824ebb20bdf723b23050ed376b6f
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-04 15:00:27 +1300

    Updated docstring and import

--- pyrisk/data/__init__.py
+++ pyrisk/data/__init__.py
@@ -3,6 +3,7 @@ pyrisk.data module
 
 submodules:
 - db: contains functions for database creation, reading, and exporting.
+- preprocessing: contains functions for preprocessing data.
 """
 
-from pyrisk.data import db
+from pyrisk.data import db, preprocessing
