97c4f6a4fe4cf90fa90f9912f10ee30ba8a8d4f3
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-04 13:51:37 +1300

    Added functions to load and save a model

--- pyrisk/models.py
+++ pyrisk/models.py
@@ -7,11 +7,15 @@ Functions:
 - create_model: Creates a new model.
 - train_model: Trains a given model on some train data.
 - test_model: Tests a model on some test data.
+- save_model: Pickles a model.
+- load_model: Loads a pickled model.
 """
+
+import pickle
 
 import sklearn as skl
 
-from pyrisk.utils.exceptions import array_check, array_dim_check
+from pyrisk.utils.exceptions import array_check, array_dim_check, file_checks
 
 
 def create_model(model_type: str, **config_params: dict):
@@ -107,7 +111,7 @@ def test_model(model, X, y, sample_weight=None) -> float:
     Parameters
     ----------
     model : skl.ensemble.HistGradBoostingClassifier or skl.svm.SVC
-        Model to train.
+        Model to test.
     X : array-like of shape (n_samples, n_features)
         Testing data.
     y : array-like of shape (n_samples, n_classes)
@@ -138,3 +142,71 @@ def test_model(model, X, y, sample_weight=None) -> float:
         array_check(sample_weight)
 
     return model.score(X, y, sample_weight=sample_weight)
+
+
+def save_model(model, save_file) -> None:
+    """
+    Saves an AI model to a pkl file.
+
+    Parameters
+    ----------
+    model : skl.ensemble.HistGradBoostingClassifier or skl.svm.SVC
+        Model to save.
+    save_file : str
+        Path to the file to save the model.
+
+    Returns
+    -------
+    None
+        Nothing is returned.
+
+    Raises
+    ------
+    TypeError
+        If save_file is not a str.
+    FileNotFoundError
+        If save_file does not exist.
+    IsADirectoryError
+        If save_file is a directory.
+    ValueError
+        If save_file extension is not .pkl file.
+
+    """
+    file_checks(save_file, ".pkl")
+
+    with open(save_file, "wb") as f:
+        pickle.dump(model, f)
+
+
+def load_model(load_file: str):
+    """
+    Loads an AI model from a .pkl file.
+
+    Parameters
+    ----------
+    load_file : str
+        Path to the file to load the model from.
+
+    Returns
+    -------
+    model : skl.ensemble.HistGradBoostingClassifier or skl.svm.SVC
+        Loaded model.
+
+    Raises
+    ------
+    TypeError
+        If load_file is not a str.
+    FileNotFoundError
+        If load_file does not exist.
+    IsADirectoryError
+        If load_file is a directory.
+    ValueError
+        If load_file extension is not .pkl file.
+
+    """
+    file_checks(load_file, ".pkl")
+
+    with open(load_file, "rb") as f:
+        model = pickle.load(f)
+
+    return model
