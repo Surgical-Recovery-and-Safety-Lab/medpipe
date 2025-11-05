36f87b950520935a55d310ecd3c9443bef60cdb6
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-05 17:50:54 +1300

    Added test for new preprocessing functions

--- test/test_data_preprocessing.py
+++ test/test_data_preprocessing.py
@@ -9,7 +9,9 @@ import pathlib
 
 import pytest
 
-from pyrisk.data.preprocessing import split_test_train
+from pyrisk.data.preprocessing import (convert_object_to_categorical,
+                                       extract_labels, label_encode_data,
+                                       split_test_train)
 from pyrisk.utils.io import load_data_from_csv
 
 CWD = pathlib.Path.cwd()
@@ -74,3 +76,87 @@ def test_split_test_train_type_errors(train_split, random_state):
 def test_split_test_train_bad_data(data):
     with pytest.raises(TypeError):
         split_test_train(data)
+
+
+@pytest.mark.parametrize(
+    "labels",
+    [
+        ["AGE"],
+        ["AGE", "SEX"],
+    ],
+)
+def test_extract_labels_success(labels):
+    data = load_data_from_csv(DATA_FILE)
+    extract_labels(data, labels)
+
+
+@pytest.mark.parametrize(
+    "data, labels",
+    [
+        (load_data_from_csv(DATA_FILE), "AGE"),  # str
+        (load_data_from_csv(DATA_FILE), [1, 2]),  # list(int)
+        (load_data_from_csv(DATA_FILE), 3.14),  # float
+        ("string", ["AGE"]),  # str
+        (3.14, ["AGE"]),  # float
+    ],
+)
+def test_extract_labels_type_error(data, labels):
+    with pytest.raises(TypeError):
+        extract_labels(data, labels)
+
+
+def test_extract_labels_key_error():
+    with pytest.raises(KeyError):
+        data = load_data_from_csv(DATA_FILE)
+        extract_labels(data, ["NOT_A_KEY"])
+
+
+@pytest.mark.parametrize(
+    "labels",
+    [
+        ["AGE"],
+        ["AGE", "SEX"],
+    ],
+)
+def test_label_encode_data_success(labels):
+    data = load_data_from_csv(DATA_FILE)
+    label_encode_data(data, labels)
+
+
+@pytest.mark.parametrize(
+    "data, labels",
+    [
+        (load_data_from_csv(DATA_FILE), "AGE"),  # str
+        (load_data_from_csv(DATA_FILE), [1, 2]),  # list(int)
+        (load_data_from_csv(DATA_FILE), 3.14),  # float
+        ("string", ["AGE"]),  # str
+        (3.14, ["AGE"]),  # float
+    ],
+)
+def test_label_encode_data_type_error(data, labels):
+    with pytest.raises(TypeError):
+        label_encode_data(data, labels)
+
+
+def test_label_encode_data_key_error():
+    with pytest.raises(KeyError):
+        data = load_data_from_csv(DATA_FILE)
+        label_encode_data(data, ["NOT_A_KEY"])
+
+
+def test_convert_object_to_categorical_success():
+    data = load_data_from_csv(DATA_FILE)
+    convert_object_to_categorical(data)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        "string",  # str
+        3.14,  # float
+        42,  # int
+    ],
+)
+def test_convert_object_to_categorical_type_error(data):
+    with pytest.raises(TypeError):
+        convert_object_to_categorical(data)
