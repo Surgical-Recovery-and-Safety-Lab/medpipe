05535c99f5ad1388db14f3228a4d08434b72c8d6
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-05 15:01:35 +1300

    Removed unused function

--- pyrisk/utils/io.py
+++ pyrisk/utils/io.py
@@ -6,7 +6,6 @@ handling various common I/O tasks.
 
 Functions:
 - load_data_from_csv: Loads the data from a .csv file.
-- save_data_to_csv: Saves a DataFrame or Series to a .csv file.
 - read_toml_configuration: Parses the contents of a .TOML file.
 """
 
@@ -50,48 +49,6 @@ def load_data_from_csv(data_file: str):
 
     data = pd.read_csv(data_file)
     return data
-
-
-def save_data_to_csv(df: pd.DataFrame, save_file: str) -> None:
-    """
-    Save data from a DataFrame to a .csv file.
-
-    Parameters
-    ----------
-    df
-        DataFrame to save.
-    save_file
-        Path and file name to save to.
-
-    Returns
-    -------
-    None
-        Nothing is returned.
-
-    Raises
-    ------
-    TypeError
-        If save_file is not a str.
-    TypeError
-        If df is not a pd.DataFrame or pd.Series.
-    IsADirectoryError
-        If save_file is not a file.
-    ValueError
-        If save_file extension is not .csv file.
-
-    """
-    try:
-        exceptions.file_checks(save_file, ".csv")
-    except (TypeError, ValueError, IsADirectoryError):
-        raise
-    except FileNotFoundError:
-        # File will be created so skip FileNotFoundError
-        pass
-
-    if type(df) is not pd.DataFrame:
-        raise TypeError("df should be a pd.DataFrame")
-
-    df.to_csv(save_file)
 
 
 def read_toml_configuration(config_file: str) -> dict:
