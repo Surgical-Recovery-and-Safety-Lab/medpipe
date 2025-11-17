65be1fc703091b4fd73ef44e60d60d9902db3010
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-18 12:14:20 +1300

    Added new core submodule

--- pyrisk/metrics/__init__.py
+++ pyrisk/metrics/__init__.py
@@ -2,7 +2,8 @@
 pyrisk.metrics module
 
 submodules:
+- core: contains functions to compute and print metrics.
 - plots: contains functions to plot results.
 """
 
-from pyrisk.metrics import plots
+from pyrisk.metrics import core, plots
