b1d681b3ea6c191f3ab0b4ef0ec319debfd96f37
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-11-19 13:39:01 +1300

    Updated logger with exception and info handlers

--- pyrisk/utils/logger.py
+++ pyrisk/utils/logger.py
@@ -5,11 +5,14 @@ This module provides helper functions for logging.
 
 Functions:
 - setup_logger: sets up a logger for a script.
+- exception_handler: Function that logs exceptions.
+- info_handler: Function that logs info.
 """
 
 import logging
 import logging.config
 import pathlib
+import sys
 
 import pyrisk.utils.exceptions
 
@@ -83,3 +86,51 @@ def setup_logger(script_name: str, log_path: str) -> logging.Logger:
     logging.config.dictConfig(logger_config_dict)  # Configure logger
 
     return logging.getLogger(script_name)
+
+
+def exception_handler(logger, log_path, log_config, script_name):
+    """
+    Handles exceptions and logs them.
+
+    Parameters
+    ----------
+    logger : logging.Logger
+        Logger object that logs the exception.
+    log_path : str
+        Path to the log file being used.
+    log_config : dict
+        Configuration parameters for the log messages.
+    script_name : str
+        Name of the script in which the error occurred.
+
+    Returns
+    -------
+    None
+        Nothing is returned.
+
+    """
+    logger.exception(log_config["log_message"] + f"{script_name}")
+    sys.stderr.write(log_config["print_message"] + f"{log_path}/{script_name}.log")
+
+
+def info_handler(logger, message, script_name):
+    """
+    Handles info messages and logs them.
+
+    Parameters
+    ----------
+    logger : logging.Logger
+        Logger object that logs the exception.
+    message : str
+        Message to print.
+    script_name : str
+        Name of the script in which the error occurred.
+
+    Returns
+    -------
+    None
+        Nothing is returned.
+
+    """
+    logger.info(f"{message} in {script_name}")
+    sys.stdout.write(f"[INFO] {message}\n")
