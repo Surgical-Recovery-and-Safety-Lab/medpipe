fa88c6cd4b06e4e93acf6f6519f273edf30282a0
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-12-03 17:08:58 +1300

    Switched from duckdb to sqlite3

--- pyrisk/data/db.py
+++ pyrisk/data/db.py
@@ -4,29 +4,29 @@ Database functions module.
 This module provides functions to open, query, and save data from databases.
 
 Functions:
-- parquet_to_duckdb: Converts a .parquet file to a duckdb.
-- extract_data_from_duckdb: Queries a duckdb to extract data.
+- parquet_to_sqlite3: Converts a .parquet file to a sqlite3.
+- extract_data_from_sqlite3: Queries a sqlite3 to extract data.
 """
 
-import duckdb
+import sqlite3
+
+import pandas as pd
 
 import pyrisk.utils.exceptions
 
 
-def parquet_to_duckdb(
-    parquet_file: str, duckdb_file: str, table_name: str = "main"
-) -> None:
+def parquet_to_db(parquet_file: str, db_file: str, table_name: str = "main") -> None:
     """
-    Converts a .parquet file to a .duckdb file.
+    Converts a .parquet file to a .db file.
 
     Parameters
     ----------
     parquet_file
         File path to the .parquet file.
-    duckdb_file
-        File path to the .duckdb file.
+    db_file
+        File path to the .db file.
     table_name, default: 'main'
-        Name of the table to create in the duckdb database.
+        Name of the table to create in the SQL database.
 
     Returns
     -------
@@ -36,15 +36,15 @@ def parquet_to_duckdb(
     Raises
     ------
     TypeError
-        If parquet_file or duckdb_file are not str.
+        If parquet_file or db_file are not str.
     FileNotFoundError
         If parquet_file does not exist.
     IsADirectoryError
-        If parquet_file or duckdb_file are not a file.
+        If parquet_file or db_file are not a file.
     ValueError
         If parquet_file extension is not a .parquet file.
     ValueError
-        If duckdb_file extension is not a .duckdb file.
+        If db_file extension is not a .sqlite3 file.
 
     """
     try:
@@ -53,63 +53,62 @@ def parquet_to_duckdb(
         raise
 
     try:
-        pyrisk.utils.exceptions.file_checks(duckdb_file, ".duckdb")
+        pyrisk.utils.exceptions.file_checks(db_file, ".db", exists=False)
     except (TypeError, ValueError, IsADirectoryError):
         raise
-    except FileNotFoundError:
-        # Duckdb will be created so pass this exception
-        pass
 
-    conn = duckdb.connect(duckdb_file)
+    conn = sqlite3.connect(db_file)
+    df = pd.read_parquet(parquet_file)
 
     drop_table = f"DROP TABLE IF EXISTS {table_name}"
-    query = f"CREATE TABLE {table_name} AS SELECT * FROM '{parquet_file}'"
 
     # Execute the queries
     conn.execute(drop_table)
-    conn.execute(query)
+    df.to_sql(table_name, conn, if_exists="replace", index=False)
+    conn.commit()
     conn.close()
 
 
-def extract_data_from_duckdb(duckdb_file: str, query: str):
+def extract_data_from_db(db_file: str, query: str):
     """
-    Extracts data from a duckdb and saves it to a .csv file.
+    Extracts data from a .db and saves it to a .csv file.
+
+    The parquet file
 
     Parameters
     ----------
-    duckdb_file
-        Path to the duckdb file.
+    db_file
+        Path to the .db file.
     query
-        Query to send to the duckdb to extract data.
+        Query to send to extract data.
 
     Returns
     -------
     data : pd.DataFrame
-        Extracted data from the duckdb database.
+        Extracted data from the database.
 
     Raises
     ------
     TypeError
-        If duckdb_file or query is not a str.
+        If db_file or query is not a str.
     FileNotFoundError
-        If duckdb_file does not exist.
+        If db_file does not exist.
     IsADirectoryError
-        If duckdb_file is not a file.
+        If db_file is not a file.
     ValueError
-        If duckdb_file extension is not a .duckdb file.
+        If db_file extension is not a .sqlite3 file.
 
     """
     try:
-        pyrisk.utils.exceptions.file_checks(duckdb_file, ".duckdb")
+        pyrisk.utils.exceptions.file_checks(db_file, ".db")
     except (FileNotFoundError, IsADirectoryError, TypeError, ValueError):
         raise
 
     if type(query) is not str:
         raise TypeError(f"{query} should be a string")
 
-    conn = duckdb.connect(duckdb_file)
-
-    df = conn.execute(query).df()  # Get the data
+    conn = sqlite3.connect(db_file)
+    df = pd.read_sql_query(query, conn)
     conn.close()
 
     return df
