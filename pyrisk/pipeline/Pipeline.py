54b2404ee9219b035a769a4ee9d2113f4e980b75
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-12-15 13:57:06 +1300

    Start of the Pipeline class

---
+++ pyrisk/pipeline/Pipeline.py
@@ -0,0 +1,99 @@
+"""
+Pipeline class.
+
+This class creates an Pipeline to prepare data, fit a predictor and
+a calibrator.
+
+"""
+
+SCRIPT_NAME = "pipeline/Pipeline"
+
+
+class Pipeline:
+    """
+    Class that creates a Pipeline.
+
+    Attributes
+    ----------
+    loader : Loader
+        Data loader object.
+    predictor : Predictor
+        Prediction model object.
+    calibrator : Calibrator
+        Calibration model object.
+    logger : logging.Logger or None, default: None
+        Logger object to log prints. If None print to terminal.
+
+    Methods
+    -------
+    __init__(loader_config, predictor_config, calibrator_config, logger)
+    fit(X, y, **kwargs)
+        Fits the predictor and calibrator.
+    predict_proba(X)
+        Predicts probabilities from predictor based on input data.
+    predict_calibrated_proba(X)
+        Predicts probabilities from calibrator based on input data.
+    """
+
+    def __init__(self, loader_config, predictor_config, calibrator_config, logger=None):
+        """
+        Initialise a Pipeline class instance.
+
+        Parameters
+        ----------
+        loader_config : dict[]
+            Configuration parameters for the loader object.
+        predictor_config : dict[]
+            Configuration parameters for the predictor object.
+        calibrator_config : dict[]
+            Configuration parameters for the calibrator object.
+        logger : logging.Logger or None, default: None
+            Logger object to log prints. If None print to terminal.
+
+        Returns
+        -------
+        None
+            Nothing is returned.
+
+        """
+        self.logger = logger
+
+    def fit(self, X, y):
+        """
+        Train the model on the provided dataset.
+
+        Parameters
+        ----------
+        X : pd.DataFrame of shape (n_samples, n_features)
+            Training data.
+        y : array-like of shape (n_samples, n_classes)
+            Prediction labels.
+
+        Returns
+        -------
+        None
+            Nothing is returned.
+
+        """
+
+    def predict_proba(self, X):
+        """
+        Predicts probabilities from predictor based on input data.
+
+        Parameters
+        ----------
+        X : pd.DataFrame of shape (n_samples, n_features)
+            Training data.
+
+        Returns
+        -------
+        probabilities : np.array (n_classes,) of arrays (n_samples, 2)
+            Predicted probabilities.
+
+        """
+        probabilities = []
+
+        if outputs.shape[1] == 1:
+            return probabilities[0]
+        else:
+            return probabilities
---
+++ pyrisk/pipeline/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyrisk.pipeline module
+
+submodules:
+- Pipeline: Pipeline class.
+"""
+
+from pyrisk.pipeline import Pipeline
