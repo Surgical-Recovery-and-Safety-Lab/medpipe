c5994b336f069ab5cf0905f4307f80ec1aa39d43
mathiasroesler <mathias.roesler@auckland.ac.nz>
2025-12-21 15:23:09 +1300

    Updated __init__

--- pyrisk/__init__.py
+++ pyrisk/__init__.py
@@ -5,7 +5,8 @@ submodules:
 - data: contains data related functions
 - metrics: contains model metric functions
 - models: contains model creation functions
+- pipeline: contains pipeline functions
 - utils: contains utility functions
 """
 
-from pyrisk import data, metrics, models, utils
+from pyrisk import data, metrics, models, pipeline, utils
