f4539be2b9d0cdf12609babdc15c8592b82364f0
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-01-06 10:57:14 +1300

    Wrote tests for the new weighting function

--- test/test_data_weighting.py
+++ test/test_data_weighting.py
@@ -11,7 +11,8 @@ import pytest
 
 from pyrisk.data.weighting import (
     inverse_frequency_class_weights,
-    inverse_frequency_sum_sample_weights,
+    inverse_frequency_multiclass_sample_weights,
+    inverse_frequency_single_sample_weights,
     negative_positive_ratio_class_weights,
     negative_positive_ratio_sample_weights,
 )
@@ -140,16 +141,16 @@ def test_negative_positive_ratio_class_weights_no_positive():
         (tri_labels, [[15 / 11, 1.25, 1.25], [15 / 4, 5.0, 5.0]]),
     ],
 )
-def test_inverse_frequency_sum_sample_weights_success(labels, true_weights):
-    weights = inverse_frequency_sum_sample_weights(labels)
+def test_inverse_frequency_multiclass_sample_weights_success(labels, true_weights):
+    weights = inverse_frequency_multiclass_sample_weights(labels)
     neg_weights = ~np.array(labels, dtype=bool) * true_weights[0]
     pos_weights = labels * true_weights[1]
     assert (weights == np.array(neg_weights + pos_weights)).all()
 
 
-def test_inverse_frequency_sum_sample_weights_bad_input():
+def test_inverse_frequency_multiclass_sample_weights_bad_input():
     with pytest.raises(ValueError):
-        _ = inverse_frequency_sum_sample_weights(labels_empty)
+        _ = inverse_frequency_multiclass_sample_weights(labels_empty)
 
 
 @pytest.mark.parametrize(
@@ -161,14 +162,54 @@ def test_inverse_frequency_sum_sample_weights_bad_input():
         ["a", "b", "c"],
     ],
 )
-def test_inverse_frequency_sum_sample_weights_type_error(labels):
+def test_inverse_frequency_multiclass_sample_weights_type_error(labels):
     with pytest.raises(TypeError):
-        _ = inverse_frequency_sum_sample_weights(labels)
+        _ = inverse_frequency_multiclass_sample_weights(labels)
 
 
-def test_inverse_frequency_sum_sample_weights_no_positive():
+def test_inverse_frequency_multiclass_sample_weights_no_positive():
     with pytest.raises(ZeroDivisionError):
-        _ = inverse_frequency_sum_sample_weights(labels_no_minority)
+        _ = inverse_frequency_multiclass_sample_weights(labels_no_minority)
+
+
+# Basic functionality tests
+@pytest.mark.parametrize(
+    "labels, pos_weight",
+    [
+        (single_labels, 2.0),
+        (bi_labels, 3.0),
+        (tri_labels, 2.0),
+    ],
+)
+def test_inverse_frequency_single_sample_weights_success(labels, pos_weight):
+    weights = inverse_frequency_single_sample_weights(labels)
+    pos_examples = np.sum(labels, axis=1) > 0
+    true_weights = np.ones(len(labels))
+    true_weights[pos_examples] *= pos_weight
+    assert (weights == true_weights).all()
+
+
+def test_inverse_frequency_single_sample_weights_bad_input():
+    with pytest.raises(ValueError):
+        _ = inverse_frequency_single_sample_weights(labels_empty)
+
+
+@pytest.mark.parametrize(
+    "labels",
+    [
+        42,
+        3.14,
+        None,
+    ],
+)
+def test_inverse_frequency_single_sample_weights_type_error(labels):
+    with pytest.raises(TypeError):
+        _ = inverse_frequency_single_sample_weights(labels)
+
+
+def test_inverse_frequency_single_sample_weights_no_positive():
+    with pytest.raises(ZeroDivisionError):
+        _ = inverse_frequency_single_sample_weights(labels_no_minority)
 
 
 # Basic functionality tests
