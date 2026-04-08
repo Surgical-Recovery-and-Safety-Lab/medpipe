760c9c0969bfcca3e9a7cd6f908bcf7ade8df1d2
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-04-08 15:55:01 +1200

    Added tests for the create_calibrator function

--- test/test_models_calibrators.py
+++ test/test_models_calibrators.py
@@ -4,7 +4,11 @@ import numpy as np
 import pytest
 
 # Assuming your file is named calibrators.py
-from medpipe.models.calibrators import IsotonicCalibrator, LogisticCalibrator
+from medpipe.models.calibrators import (
+    IsotonicCalibrator,
+    LogisticCalibrator,
+    create_calibrator,
+)
 
 # --- Fixtures ---
 
@@ -75,3 +79,23 @@ def test_cannot_instantiate_base_calibrator():
 
     with pytest.raises(TypeError, match="Can't instantiate abstract class"):
         BaseCalibrator(model_type="logistic")
+
+
+@pytest.mark.parametrize(
+    "model_type, output_model_type",
+    [("isotonic", IsotonicCalibrator), ("logistic", LogisticCalibrator)],
+)
+def test_create_calibrator_success(model_type, output_model_type):
+    """Test that the create_calibrator function creates calibrators"""
+    calibrator = create_calibrator(model_type)
+
+    assert type(calibrator) is output_model_type
+
+
+def test_create_calibrator_value_error():
+    """Test that the create_calibrator function throws a ValueError when wrong type"""
+    with pytest.raises(
+        ValueError,
+        match="model type should be isotonic or logistic, but got wrong_type",
+    ):
+        create_calibrator("wrong_type")
