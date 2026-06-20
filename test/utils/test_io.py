8a40b53f6f947844f51624fb289fd5b9e263d060
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-06-20 16:00:26 -0700

    Wrote tests for the load_data function

--- test/utils/test_io.py
+++ test/utils/test_io.py
@@ -6,12 +6,14 @@ I/O functions and classes tests suite.
 
 from pathlib import Path
 
+import pandas as pd
 import pytest
 
-from medpipe.utils.io import read_toml_configuration
+from medpipe.utils.io import load_data, read_toml_configuration
 
 
 class TestReadTOMLConfiguration:
+    """Test class for the read_toml_configuration function."""
 
     @pytest.fixture
     def example_config_dir(self) -> Path:
@@ -23,3 +25,23 @@ class TestReadTOMLConfiguration:
     def test_read_configuration(self, example_config_dir: Path) -> None:
         """Test successfull function call."""
         read_toml_configuration(example_config_dir / "HGBc_config.toml")
+
+
+class TestLoadData:
+    """Test class for the load_data function."""
+
+    @pytest.fixture
+    def write_data(self, tmp_path: Path) -> None:
+        """Write a csv and parquet file to test loading."""
+        dummy_data = pd.DataFrame([0, 1, 2, 3])
+        dummy_data.to_csv(tmp_path / "dummy.csv")
+        dummy_data.to_parquet(tmp_path / "dummy.parquet")
+
+    @pytest.mark.parametrize("file", ["dummy.csv", "dummy.parquet"])
+    def test_load_data_success(
+        self, tmp_path: Path, file: str, write_data: None
+    ) -> None:
+        """Test successfull function call."""
+        data = load_data(tmp_path / file)
+
+        assert isinstance(data, pd.DataFrame)
