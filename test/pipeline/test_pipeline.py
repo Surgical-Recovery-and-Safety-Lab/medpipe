2579891ac72ba8315c52e72b69d2c5b281cb9e5a
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-06-20 17:48:44 -0700

    Started writing tests for Pipeline class

---
+++ test/pipeline/test_pipeline.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Pipeline class tests suite.
+"""
+
+from pathlib import Path
+
+import pytest
+
+from medpipe import Pipeline
+
+
+class TestPipline:
+    """Test class for the Pipline class"""
+
+    @pytest.fixture
+    def example_config_dir(self) -> Path:
+        """Provide the location of the example configuration files."""
+        base_dir = Path(__file__).parent.parent.parent
+
+        return base_dir / "config-examples/"
+
+    def test_create_pipeline(self, example_config_dir: Path) -> None:
+        """Test successful pipeline creation."""
+        Pipeline(example_config_dir / "HGBc_config.toml")
