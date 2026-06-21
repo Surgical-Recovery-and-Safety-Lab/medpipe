2746686cdaefcd3d831b8005e8d2f1b64dc708a9
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-06-20 19:46:35 -0700

    Wrote test functions for the get_positive_proba function

--- test/models/test_core.py
+++ test/models/test_core.py
@@ -6,19 +6,28 @@ Models core module test suite.
 
 from pathlib import Path
 from re import escape
-from typing import Any, TypeAlias
+from typing import TYPE_CHECKING, Any, TypeAlias
 
+import numpy as np
 import pytest
 from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.isotonic import IsotonicRegression
 from sklearn.linear_model import LogisticRegression
 
-from medpipe.models.core import create_model, load_pipeline, save_pipeline
+from medpipe._types import FullProba, PosProba
+from medpipe.models.core import (
+    create_model,
+    get_positive_proba,
+    load_pipeline,
+    save_pipeline,
+)
 from medpipe.pipeline.pipeline import Pipeline
 
 ModelTypes: TypeAlias = type[
     HistGradientBoostingClassifier | IsotonicRegression | LogisticRegression
 ]
+
+from numpy.typing import NDArray
 
 
 class TestCreateModel:
@@ -126,3 +135,43 @@ class TestLoadPipeline:
         loaded_pipeline = load_pipeline(tmp_path / "pipeline.joblib")
 
         assert isinstance(loaded_pipeline, Pipeline)
+
+
+class TestGetPositiveProba:
+    """Test class for the get_positive_proba function."""
+
+    @pytest.mark.parametrize(
+        "full_proba, expected_proba",
+        [
+            (
+                np.array([[0, 1], [0.5, 0.5], [0.9, 0.1]]),
+                np.array([[1], [0.5], [0.1]]),
+            ),
+            (
+                [np.array([[0, 1]]), np.array([[0.5, 0.5]])],
+                [np.array([1, 0.5])],
+            ),
+            (
+                [np.array([[0, 1], [0.5, 0.5]]), np.array([[0.5, 0.5], [0.9, 0.1]])],
+                [np.array([[1, 0.5], [0.5, 0.1]])],
+            ),
+        ],
+    )
+    def test_get_positive_proba_success(
+        self, full_proba: FullProba | list[NDArray], expected_proba: PosProba
+    ) -> None:
+        """Test successful function call."""
+        pos_proba = get_positive_proba(full_proba)
+        assert (pos_proba == expected_proba).all()
+
+    def test_get_positive_proba_mismatch(self) -> None:
+        """Test case when list has mismatched lenghts."""
+        mismatched_proba = [
+            np.array([[0.2, 0.8], [1, 0]]),
+            np.array([[0.1, 0.9]]),
+        ]
+        match_expr = "setting an array element with a sequence. "
+        "The requested array has an inhomogeneous shape after 1 dimensions. "
+        "The detected shape was (2,) + inhomogeneous part."
+        with pytest.raises(ValueError, match=match_expr):
+            get_positive_proba(mismatched_proba)
