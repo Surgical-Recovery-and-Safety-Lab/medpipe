9ca088ffb31285f32c953b54da072d70a6d7d0e1
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-06-25 08:57:20 +0200

    Simplified tests by removing overkill fixtures

--- test/utils/test_io.py
+++ test/utils/test_io.py
@@ -27,33 +27,25 @@ class TestReadTOMLConfiguration:
             # Relinquish control back to the test runner loop
             yield mock_path_mkdir, mock_os_makedirs
 
-    @pytest.fixture
-    def example_config_dir(self) -> Path:
-        """Provide the location of the example configuration files."""
+    def test_read_configuration(self) -> None:
+        """Test successful function call."""
         base_dir = Path(__file__).parent.parent.parent
 
-        return base_dir / "config-examples/"
-
-    def test_read_configuration(self, example_config_dir: Path) -> None:
-        """Test successful function call."""
+        example_config_dir = base_dir / "config-examples/"
         read_toml_configuration(example_config_dir / "HGBc_config.toml")
 
 
 class TestLoadData:
     """Test class for the load_data function."""
 
-    @pytest.fixture
-    def write_data(self, tmp_path: Path) -> None:
-        """Write a csv and parquet file to test loading."""
+    @pytest.mark.parametrize("file", ["dummy.csv", "dummy.parquet"])
+    def test_load_data_success(self, tmp_path: Path, file: str) -> None:
+        """Test successful function call."""
+        # Write some dummy data
         dummy_data = pd.DataFrame([0, 1, 2, 3])
         dummy_data.to_csv(tmp_path / "dummy.csv")
         dummy_data.to_parquet(tmp_path / "dummy.parquet")
 
-    @pytest.mark.parametrize("file", ["dummy.csv", "dummy.parquet"])
-    def test_load_data_success(
-        self, tmp_path: Path, file: str, write_data: None
-    ) -> None:
-        """Test successful function call."""
         data = load_data(tmp_path / file)
 
         assert isinstance(data, pd.DataFrame)
