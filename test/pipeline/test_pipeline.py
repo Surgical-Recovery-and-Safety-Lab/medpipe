3123605ed71445e1c3ee1c016e3feddbab2cdb63
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-06-25 07:17:23 +0200

    Fixed creation of models and figures folder when running tests

--- test/pipeline/test_pipeline.py
+++ test/pipeline/test_pipeline.py
@@ -5,10 +5,23 @@ Pipeline class tests suite.
 """
 
 from pathlib import Path
+from typing import Any, Generator
+from unittest.mock import patch
 
 import pytest
 
 from medpipe.pipeline.pipeline import MedpipePipeline
+
+
+@pytest.fixture(autouse=True)
+def shield_local_filesystem() -> Generator[Any, Any, Any]:
+    """Automatically prevent any test in this file from creating folders on disk."""
+    with (
+        patch("pathlib.Path.mkdir") as mock_path_mkdir,
+        patch("os.makedirs") as mock_os_makedirs,
+    ):
+        # Relinquish control back to the test runner loop
+        yield mock_path_mkdir, mock_os_makedirs
 
 
 class TestPipline:
--- test/utils/test_io.py
+++ test/utils/test_io.py
@@ -5,6 +5,8 @@ I/O functions and classes tests suite.
 """
 
 from pathlib import Path
+from typing import Any, Generator
+from unittest.mock import patch
 
 import pandas as pd
 import pytest
@@ -14,6 +16,16 @@ from medpipe.utils.io import load_data, read_toml_configuration
 
 class TestReadTOMLConfiguration:
     """Test class for the read_toml_configuration function."""
+
+    @pytest.fixture(autouse=True)
+    def shield_local_filesystem(self) -> Generator[Any, Any, Any]:
+        """Automatically prevent any test in this file from creating folders on disk."""
+        with (
+            patch("pathlib.Path.mkdir") as mock_path_mkdir,
+            patch("os.makedirs") as mock_os_makedirs,
+        ):
+            # Relinquish control back to the test runner loop
+            yield mock_path_mkdir, mock_os_makedirs
 
     @pytest.fixture
     def example_config_dir(self) -> Path:
