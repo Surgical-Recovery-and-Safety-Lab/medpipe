0ac28afa47ca1aee42fdf0d6661e38c3c09bf602
mathiasroesler <mathias.roesler@auckland.ac.nz>
2026-06-25 08:53:59 +0200

    Updated MedpipePipeline tests
    Moved fixtures outside of the classes
    Added test case with MedpipeConfig

--- test/pipeline/test_pipeline.py
+++ test/pipeline/test_pipeline.py
@@ -9,8 +9,14 @@ from typing import Any, Generator
 from unittest.mock import patch
 
 import pytest
+from sklearn.compose import ColumnTransformer
 
 from medpipe.pipeline.pipeline import MedpipePipeline
+from medpipe.utils.io import read_toml_configuration
+
+# ==============================================================================
+# Fixtures for all tests
+# ==============================================================================
 
 
 @pytest.fixture(autouse=True)
@@ -24,20 +30,44 @@ def shield_local_filesystem() -> Generator[Any, Any, Any]:
         yield mock_path_mkdir, mock_os_makedirs
 
 
-class TestPipline:
-    """Test class for the Pipline class"""
+@pytest.fixture
+def example_config_dir() -> Path:
+    """Provide the location of the example configuration files."""
+    base_dir = Path(__file__).parent.parent.parent
 
-    @pytest.fixture
-    def example_config_dir(self) -> Path:
-        """Provide the location of the example configuration files."""
-        base_dir = Path(__file__).parent.parent.parent
+    return base_dir / "config-examples/"
 
-        return base_dir / "config-examples/"
 
-    def test_create_pipeline(self, example_config_dir: Path) -> None:
-        """Test successful pipeline creation."""
+@pytest.fixture
+def mp_pipeline(example_config_dir: Path) -> MedpipePipeline:
+    """Create a Medpipe Pipeline for tests."""
+    return MedpipePipeline(example_config_dir / "HGBc_config.toml", logger=None)
+
+
+# ==============================================================================
+# Test classes
+# ==============================================================================
+
+
+class TestPipeline:
+    """Test class for the MedpipePipeline class"""
+
+    def test_create_pipeline_from_file(self, example_config_dir: Path) -> None:
+        """Test successful pipeline creation from configuration file."""
         pipe = MedpipePipeline(example_config_dir / "HGBc_config.toml", logger=None)
         assert pipe.version == "v0.1.1"
         assert pipe.predictor_algo == "HistGradientBoostingClassifier"
         assert pipe.calibrator_method == "IsotonicRegression"
         assert pipe.n_outcomes == 1
+        assert isinstance(pipe.preprocessor, ColumnTransformer)
+
+    def test_create_pipeline_from_config(self, example_config_dir: Path) -> None:
+        """Test successful pipeline creation from MedpipeConfig."""
+        config = read_toml_configuration(example_config_dir / "HGBc_config.toml")
+        pipe = MedpipePipeline(config, logger=None)
+        assert pipe.version == "v0.1.1"
+        assert pipe.predictor_algo == "HistGradientBoostingClassifier"
+        assert pipe.calibrator_method == "IsotonicRegression"
+        assert pipe.n_outcomes == 1
+        assert isinstance(pipe.preprocessor, ColumnTransformer)
+
